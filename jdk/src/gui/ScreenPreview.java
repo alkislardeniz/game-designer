fc2b98b5bef7202dcb3022ce910a8461b5883181
Mehmet <mehmetcc@outlook.com>
2016-04-26 14:35:23 +0300

    Major changes in ScreenPreview

--- jdk/src/gui/ScreenPreview.java
+++ jdk/src/gui/ScreenPreview.java
@@ -2,6 +2,8 @@ package gui;
 
 import gamemodel.*;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Displays preview image for screens, to be used inside GameEditController.
@@ -11,9 +13,64 @@ public abstract class ScreenPreview extends JPanel
 {
     Screen screen;
 
+    //properties
+    private String name;
+
     // for assignment screens, show the screen's name and assignment
     // for conditional screens, show its name and predicate
     // for playable screens, show its name
+    public ScreenPreview(Screen screen)
+    {
+        this.screen = screen;
+        name = screen.getName();
+
+
+
+        if (screen.getPlayable())
+        {
+            JLabel nameLabel = new JLabel(name);
+            add(nameLabel);
+        }
+        else if (screen.getClass().equals("gamemodel.CondScreen"))
+        {
+            CondScreen condScreen = (CondScreen) screen;
+            String name = condScreen.getName();
+            String condString = condScreen.getPred().toString();
+
+            JLabel nameLabel = new JLabel(name);
+            JLabel condLabel = new JLabel(condString);
+
+            add(nameLabel);
+            add(condLabel);
+        }
+        else if (screen.getClass().equals("gamemodel.AssignScreen"))
+        {
+            AssignScreen assignScreen = (AssignScreen) screen;
+            String name = assignScreen.getName();
+            String assignment = assignScreen.getName();
+
+            JLabel nameLabel = new JLabel(name);
+            JLabel assignLabel = new JLabel(assignment);
+
+            add(nameLabel);
+            add(assignLabel);
+        }
+    }
+
 
     // use clicks and double clicks to open a dialog to edit properties or the corresponding ScreenEditController
+    public class MouseClicked extends MouseAdapter
+    {
+        public void MouseClicked(MouseEvent e)
+        {
+            if (e.getClickCount() == 2)
+            {
+                // ToDo
+            }
+            else
+            {
+
+            }
+        }
+    }
 }
