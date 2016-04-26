8513de5a697bfe62972e338f49e3bde4d1010804
Mehmet <mehmetcc@outlook.com>
2016-04-26 14:38:12 +0300

    Major changes in ScreenPreview

--- jdk/src/gui/ScreenPreview.java
+++ jdk/src/gui/ScreenPreview.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseEvent;
 
 /**
  * Displays preview image for screens, to be used inside GameEditController.
+ * Mehmet was here on 26 April 2016
  * Created by admin on 4/26/16.
  */
 public abstract class ScreenPreview extends JPanel
