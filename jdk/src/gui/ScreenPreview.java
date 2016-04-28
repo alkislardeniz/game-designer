4a5827bbb04334eca9862a7aab7b12f150ee0492
Ata Deniz Aydin <atadenizaydin@gmail.com>
2016-04-28 17:54:14 +0300

    Created dialogs for adding screens and components

--- jdk/src/gui/ScreenPreview.java
+++ jdk/src/gui/ScreenPreview.java
@@ -74,6 +74,8 @@ public class ScreenPreview extends JPanel implements ScreenVisitor
         add(textLabel);
     }
 
+    // TODO JLabel does not change
+
     class ScreenDialog implements ScreenVisitor
     {
         public void visit(PlayableScreen screen) {}
