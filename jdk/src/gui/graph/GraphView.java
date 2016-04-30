1ba8d423a20f4c23ed35f60bc77a95cb90c252ec
Ata Deniz Aydin <atadenizaydin@gmail.com>
2016-04-30 13:52:35 +0300

    Added graph classes

--- jdk/src/gui/graph/GraphView.java
+++ jdk/src/gui/graph/GraphView.java
@@ -1,6 +1,5 @@
 package gui.graph;
 
-import java.util.LinkedList;
 
 /**
  * Represents a graph graphically.
@@ -9,13 +8,12 @@ import java.util.LinkedList;
 public class GraphView<E>
 {
     Graph<E> graph;
-    LinkedList<Integer> queue; // store queue of x-coordinate offsets
+    Grid<E> grid;
 
     public GraphView(Graph<E> graph)
     {
         this.graph = graph;
-        queue = new LinkedList<>();
-        queue.add(0);
+
     }
 
     // TODO add and remove E, draw each GraphEdge<E> between two Es based on first available slot in queue
@@ -24,4 +22,10 @@ public class GraphView<E>
 
     // Better yet, use neighborhoods that fill topmost available space, going left if right is full,
     // right if left is full, and also count lines as filling a neighborhood
+
+    // Algorithm:
+    // - Add each screen in sequence, starting from the start screen at the top
+    // - For each screen, add its next screens in sequence
+    // - If the next screen isn't added already, add it to first available neighbor with most neighbors and draw edge
+    // - If it is added already, draw edge from it to next screen
 }
